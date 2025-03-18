304caf894626c6f5fd94dbc4aeabba48d4dde529
himalMajumder <himalmajumder@gmail.com>
2025-03-18 23:58:10 +0600

    some change

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,31 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    base: "./",
+    build: {
+        chunkSizeWarningLimit: 2000,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return "vendor";
+                    }
+                },
+            },
+        },
     },
-  },
+    server: {
+        host: "::",
+        port: 8080,
+    },
+    plugins: [
+        react(),
+        mode === 'development' &&
+        componentTagger(),
+    ].filter(Boolean),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 }));
